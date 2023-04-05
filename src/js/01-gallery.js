54c9a1081b302c5441a6f499e0e28dcc74cc6051
Juliusz Janicki <juliusz.janicki@gmail.com>
2023-04-05 21:04:25 +0200

    part 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,38 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+// for (const item of galleryItems) {}
+
+galleryItems.forEach(item => {
+  const imageItem = document.createElement('li');
+  galleryEl.append(imageItem);
+
+  imageItem.insertAdjacentHTML(
+    'beforeend',
+    `<a class="gallery__link" href="${item.original}">
+  <img
+  class="gallery__image"
+  src="${item.preview}"
+  data-source="${item.original}"
+  alt="${item.description}"
+/>
+</a>`
+  );
+});
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 200,
+});
+
+// galleryEl.innerHTML = "";
+
+// galleryEl.innerHTML += newImage;
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash';
 
+const player = new Player('vimeo-player');
+
+player.on(
+  'timeupdate',
+  _.throttle(function (data) {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000)
+);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
