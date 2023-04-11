3a82f2f71d9f42ea052fa68787f589aec226955b
Juliusz Janicki <juliusz.janicki@gmail.com>
2023-04-11 17:49:14 +0200

    done
    part2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,23 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash';
 
+// instalacja node.js lodash
+import throttle from 'lodash.throttle';
+
+// inicjowanie odtwarzania w skrypcie
 const player = new Player('vimeo-player');
 
+// zdarzenie 'timeupdate' + zapisywanie czasu odtwarzania w local storeage
+// throttle -> ograniczenie częstotliwości aktualizacji
+// data -> aktualny czas odtwarzania i zapisujemy go pod kluczem v-c-t
 player.on(
   'timeupdate',
-  _.throttle(function (data) {
+  throttle(function (data) {
     const currentTime = data.seconds;
     localStorage.setItem('videoplayer-current-time', currentTime);
   }, 1000)
 );
+
+// zapisany czas ładuje sie podczas oświezenia strony
 
 const savedTime = localStorage.getItem('videoplayer-current-time');
 if (savedTime) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,67 @@
+import throttle from 'lodash.throttle';
+// const formEl = document.querySelector('.feedback-form');
+// const emailEl = document.querySelector('input[name="email"]');
+// const messageEl= document.querySelector('textarea[name="message"]');
+// zamiast metody łańcuchowej (powyżej) pobieram pozycje bezpośrednio z forumlarza
 
+// dowiedziec się która metoda jest szybsza
+
+const feedbackEl = document.querySelector('.feedback-form');
+const emailInput = feedbackEl.querySelector('input[name="email"]');
+const messageInput = feedbackEl.querySelector('textarea[name="message"]');
+
+// uzyłem lodash.throttle, do ustawienia częstoliwości aktualizacji localstorage
+// 500milisekund ;]
+
+const saveForm = throttle(() => {
+  const stateForm = {
+    email: emailInput.value,
+    message: messageInput.value,
+
+    // wartości inputów
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(stateForm));
+}, 500);
+
+// zdarzenie zapisania danych z localstorage po odświeżeniu strony
+// JSON.parse -> parsowanie -> odczyt danych z local'a
+
+feedbackEl.addEventListener('input', saveForm);
+
+const stateLSForm = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (stateLSForm) {
+  emailInput.value = stateLSForm.email;
+  messageInput.value = stateLSForm.message;
+}
+// zdarzenie wymazania danych z localstorage i inputa po sumbicie
+
+feedbackEl.addEventListener('submit', event => {
+  event.preventDefault();
+  const stateForm = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(stateForm);
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+
+  // puste pola po naciśnięciu sumbit'a
+});
+
+// przykład fu try/catch z odczytem localstorage'a
+// function xyz() {
+//     try {
+//       const zyx = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+//       if (zyx) {
+//         emailInput.value = zyx.email || '';
+//         messageInput.value = zyx.message || '';
+//       }
+//     } catch (err) {
+//       console.error('coś tam')
+//   }
+
+//   xyz();
+
+// nie wiem która metoda odczytania będzie bardziej czysta... -> dopytać
